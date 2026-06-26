937dec5eba45865f9584619cda21b215d199bf9c
mohanteja781112 <doddimohanteja711@gmail.com>
2026-06-26 14:45:02 +0530

    Increase scale of Hero Visual orbit on mobile screens

--- frontend/src/components/HeroVisual.jsx
+++ frontend/src/components/HeroVisual.jsx
@@ -34,7 +34,7 @@ const HeroVisual = () => {
 
   return (
     <div 
-      className="relative w-full max-w-[500px] aspect-square flex items-center justify-center scale-[0.35] sm:scale-75 md:scale-[0.60] lg:scale-[0.70] xl:scale-[0.85] 2xl:scale-100 transition-transform duration-500 origin-center -my-[130px] sm:-my-[60px] md:-my-[40px] lg:my-0"
+      className="relative w-full max-w-[500px] aspect-square flex items-center justify-center scale-[0.50] sm:scale-75 md:scale-[0.60] lg:scale-[0.70] xl:scale-[0.85] 2xl:scale-100 transition-transform duration-500 origin-center -my-[110px] sm:-my-[60px] md:-my-[40px] lg:my-0"
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
     >
